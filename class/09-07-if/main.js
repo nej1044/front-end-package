6ffdea77467abd166f5d880794b3198169ff37e2
nej1044 <nej1044@naver.com>
2022-08-18 15:45:19 +0900

    day09-if/dom-api

---
+++ class/09-07-if/main.js
@@ -0,0 +1,19 @@
+let isShow = true;
+let checked = false;
+
+if(isShow) {
+    console.log('Show') // Show
+}
+
+if(checked) {
+    console.log('Checked')
+}
+
+let isShow = true;
+
+if(isShow) {
+    console.log('Show')
+} else {
+    console.log('Hide')
+}
+
